d1c6af8ed048c1057edda99f0b711c34e1892455
Yu Kurebayashi <y_kurebayashi@uluru.jp>
2020-09-05 20:53:35 +0900

    API追加&ローカル環境修正

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  index(): object {
+    return this.appService.index();
+  }
+
+  @Get('/:id')
+  get(): object {
+    return this.appService.get();
   }
 }
